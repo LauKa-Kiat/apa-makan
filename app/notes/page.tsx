5d15f8be992d93b4d6e2d402ecc103a2d7683d90
LauKa-Kiat <gabriel.lkk02@gmail.com>
2026-01-12 17:05:58 +0800

    feat: add ProtectedLayout component with navigation and footer

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -3,6 +3,5 @@ import { createClient } from "@/lib/supabase/server";
 export default async function Page() {
   const supabase = await createClient();
   const { data: notes } = await supabase.from("places").select();
-
   return <pre>{JSON.stringify(notes, null, 2)}</pre>;
 }
